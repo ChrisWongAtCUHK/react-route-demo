e85bbaf88e97a0ad375c2aef1d56fb414f02b3b0
Chris Wong <wc38.wong@gmail.com>
2020-04-02 18:07:41 +0800

    Updates

--- precache-manifest.cb8ec6fd692e6b2d54d705b4dfb0abe5.js
+++ precache-manifest.cb8ec6fd692e6b2d54d705b4dfb0abe5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "02cefc1361d3c9810102fa0082f6fa74",
+    "revision": "a01346834c069ae7625035fdfcc03de9",
     "url": "/react-route-demo/index.html"
   },
   {
-    "revision": "4c6081dd758f318c4d6c",
+    "revision": "33684dffce775ef30b2b",
     "url": "/react-route-demo/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-route-demo/static/js/2.f3438296.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4c6081dd758f318c4d6c",
-    "url": "/react-route-demo/static/js/main.eb59ef73.chunk.js"
+    "revision": "33684dffce775ef30b2b",
+    "url": "/react-route-demo/static/js/main.acca7320.chunk.js"
   },
   {
     "revision": "01064ae9ad803091b7ff",
